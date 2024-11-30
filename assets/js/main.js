b468a71e4e70972eb9dc940902bd60894b7ef3f8
Guru <guru85111@gmail.com>
2024-11-30 15:20:52 +0530

    "Updated CSS styles for popular sections."

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,33 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const changeBackground = () => {
+  const header = document.getElementById("header");
 
+  this.scrollY >= 50
+    ? header.classList.add("scroll-header")
+    : header.classList.remove("scroll-header");
+};
+window.addEventListener("scroll", changeBackground);
 
 /*=============== SWIPER POPULAR ===============*/
+var swiper = new Swiper(".popular__container", {
+    spaceBetween: 32,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPreview: true,
+    loop: true,
+    
 
-
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 /*=============== VALUE ACCORDION ===============*/
-
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+/*=============== SHOW SCROLL UP ===============*/
 
-/*=============== SHOW SCROLL UP ===============*/ 
-
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
